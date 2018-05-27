dc13e18bc83115e16d8172dc9ac217fbba61453a
Christopher Pitt <c.james.pitt@gmail.com>
2018-05-27 17:02:04 +0100

    Add SPA support to GH pages, using 404.html to catch non-found traffic.

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,34 @@ const ProfileContainer = styled.div`
   }
 `
 
+const redirect = ({ location, history }) => {
+  if (location.search) {
+    const q = {}
+    location.search
+      .slice(1)
+      .split('&')
+      .forEach(v => {
+        const a = v.split('=')
+        q[a[0]] = a
+          .slice(1)
+          .join('=')
+          .replace(/~and~/g, '&')
+      })
+    if (q.p !== undefined) {
+      history.replaceState(
+        null,
+        null,
+        `${location.pathname.slice(0, -1)}${q.p || ''}${q.q ? `?${q.q}` : ''}${
+          location.hash
+        }`,
+      )
+    }
+  }
+}
+
 export default () => {
+  if (typeof window === 'object') redirect(window)
+
   const timeline = [
     ...cvData.jobHistory.map(history => {
       return {
